44775b491f94c60b548523d0bc94bcff1bdacafe
rsuntk <rsuntk@yukiprjkt.my.id>
2025-07-05 11:05:15 +0700

    a05m: Import vendor_boot lkms
    Co-authored-by: Jamie Falconer <mrfluffyoven@gmail.com>
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,9 +102,13 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
+# GKIs hack
+TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)/")
+
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
