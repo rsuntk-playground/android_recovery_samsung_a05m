1704603d6442540ca3522d2da9f9276f99e41a86
rsuntk <rsuntk@yukiprjkt.my.id>
2025-07-07 09:44:42 +0700

    a05m: Move some flags to common
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- twrp_a05m.mk
+++ twrp_a05m.mk
@@ -13,10 +13,6 @@ $(call inherit-product, device/samsung/a05m/device.mk)
 
 PRODUCT_DEVICE := a05m
 PRODUCT_NAME := twrp_a05m
-PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A055F
-PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 BUILD_FINGERPRINT := samsung/a05mxx/a05m:15/AP3A.240905.015.A2/A055FXXU9DYF5:user/release-keys
